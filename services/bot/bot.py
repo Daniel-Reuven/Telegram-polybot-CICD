24eb7255b55e0d19af278db7425aa4339e034651
DanielAdmin <prodevildaniel@gmail.com>
2022-12-06 20:23:58 +0200

    initial

--- services/bot/bot.py
+++ services/bot/bot.py
@@ -88,7 +88,6 @@ class YoutubeObjectDetectBot(Bot):
             logger.error(error)
             self.send_text(update, f'Something went wrong, please try again...')
 
-
 if __name__ == '__main__':
     with open('.telegramToken') as f:
         _token = f.read()
