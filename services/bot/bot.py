0e8e55b2943a4f592c2ab01687dff7f0bf8f23bc
DanielAdmin <prodevildaniel@gmail.com>
2022-12-11 20:14:13 +0200

    fixed config.json path

--- services/bot/bot.py
+++ services/bot/bot.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     with open('secrets/.telegramToken') as f:
         _token = f.read()
 
-    with open('config.json') as f:
+    with open('common/config.json') as f:
         config = json.load(f)
 
     sqs = boto3.resource('sqs', region_name=config.get('aws_region'))
